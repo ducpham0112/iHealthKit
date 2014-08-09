dea9e232ecd51d1b0918185fbb5fccc20bb5b259
ducpham0112 <admin@admins-Mac.local>
2014-08-10 00:24:49 +0700

    Update Constants In Location Manager

--- iHealthKit/MyLocationManager/MyLocationManager.m
+++ iHealthKit/MyLocationManager/MyLocationManager.m
@@ -6,21 +6,18 @@
 //  Copyright (c) 2014 iOS Fresher. All rights reserved.
 //
 
-static const NSUInteger kDistanceFilter = 5; // the minimum distance (meters) for which we want to receive location updates (see docs for CLLocationManager.distanceFilter)
-static const NSUInteger kHeadingFilter = 30; // the minimum angular change (degrees) for which we want to receive heading updates (see docs for CLLocationManager.headingFilter)
-static const NSUInteger kDistanceAndSpeedCalculationInterval = 3; // the interval (seconds) at which we calculate the user's distance and speed
-static const NSUInteger kMinimumLocationUpdateInterval = 10; // the interval (seconds) at which we ping for a new location if we haven't received one yet
-static const NSUInteger kNumLocationHistoriesToKeep = 5; // the number of locations to store in history so that we can look back at them and determine which is most accurate
-static const NSUInteger kValidLocationHistoryDeltaInterval = 3; // the maximum valid age in seconds of a location stored in the location history
-static const NSUInteger kNumSpeedHistoriesToAverage = 5; // the number of speeds to store in history so that we can average them to get the current speed
-static const NSUInteger kPrioritizeFasterSpeeds = 1; // if > 0, the currentSpeed and complete speed history will automatically be set to to the new speed if the new speed is faster than the averaged speed
-static const NSUInteger kMinLocationsNeededToUpdateDistanceAndSpeed = 3; // the number of locations needed in history before we will even update the current distance and speed
-static const CGFloat kRequiredHorizontalAccuracy = 20.0; // the required accuracy in meters for a location.  if we receive anything above this number, the delegate will be informed that the signal is weak
+static const NSUInteger kDistanceFilter = 5;
+static const NSUInteger kHeadingFilter = 30;
+static const NSUInteger kDistanceAndSpeedCalculationInterval = 3;
+static const NSUInteger kMinimumLocationUpdateInterval = 10;
+static const NSUInteger kNumLocationHistoriesToKeep = 5;
+static const NSUInteger kValidLocationHistoryDeltaInterval = 3;
+static const NSUInteger kNumSpeedHistoriesToAverage = 5;
+static const NSUInteger kMinLocationsNeededToUpdateDistanceAndSpeed = 5;
+static const CGFloat kRequiredHorizontalAccuracy = 20.0;
 static const CGFloat kMediumHorizontalAccuracy = 40.0;
-static const CGFloat kMaximumAcceptableHorizontalAccuracy = 70.0; // the maximum acceptable accuracy in meters for a location.  anything above this number will be completely ignored
-static const NSUInteger kGPSRefinementInterval = 15; // the number of seconds at which we will attempt to achieve kRequiredHorizontalAccuracy before giving up and accepting kMaximumAcceptableHorizontalAccuracy
-
-static const CGFloat kSpeedNotSet = -1.0;
+static const CGFloat kMaximumHorizontalAccuracy = 70.0;
+static const NSUInteger kGPSRefinementInterval = 15;
 
 #import "MyLocationManager.h"
 
@@ -152,7 +149,7 @@ static const CGFloat kSpeedNotSet = -1.0;
         [_locationHistory removeAllObjects];
         [_speedHistory removeAllObjects];
         _lastDistanceAndSpeedCalculation = 0;
-        _currentSpeed = kSpeedNotSet;
+        _currentSpeed = -1.0;
         _readyToExposeDistanceAndSpeed = NO;
         _allowMaximumAcceptableAccuracy = NO;
         _needUpdateSignalStrength = YES;
@@ -237,7 +234,7 @@ static const CGFloat kSpeedNotSet = -1.0;
     
     double horizontalAccuracy;
     if (_allowMaximumAcceptableAccuracy) {
-        horizontalAccuracy = kMaximumAcceptableHorizontalAccuracy;
+        horizontalAccuracy = kMaximumHorizontalAccuracy;
     } else {
         horizontalAccuracy = kRequiredHorizontalAccuracy;
     }
@@ -317,15 +314,7 @@ static const CGFloat kSpeedNotSet = -1.0;
                         totalSpeed += [speedNumber doubleValue];
                     }
                     if (canUpdateDistanceAndSpeed) {
-                        double newSpeed = totalSpeed / (double)[_speedHistory count];
-                        if (kPrioritizeFasterSpeeds > 0 && speed > newSpeed) {
-                            newSpeed = speed;
-                            [_speedHistory removeAllObjects];
-                            for (int i=0; i<kNumSpeedHistoriesToAverage; i++) {
-                                [_speedHistory addObject:[NSNumber numberWithDouble:newSpeed]];
-                            }
-                        }
-                        _currentSpeed = newSpeed;
+                        _currentSpeed = totalSpeed / (double)[_speedHistory count];
                     }
                 }
             }
